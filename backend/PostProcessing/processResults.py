870dd9fb9b876f7f6c47771963c6c0344ea9e26a
Alex DeCourcy <adecourc@indiana.edu>
2019-07-28 14:49:34 -0400

    diagnostic

--- backend/PostProcessing/processResults.py
+++ backend/PostProcessing/processResults.py
@@ -245,11 +245,12 @@ def removeAdjacentPeaks(spectrum: List[Tuple[int, int]],
     return massTolerance(mz1, mz2)
   
   filteredSpectrum = spectrum[:]
-
-  print(filteredSpectrum[i][0])
-  print(filteredSpectrum[i+1][0])
   
   for i in range(len(filteredSpectrum)-1):
+    print(filteredSpectrum[i][0])
+    print(filteredSpectrum[i+1][0])
+    print(differenceCalc(filteredSpectrum[i][0], filteredSpectrum[i+1][0]))
+    print(precision)
     if differenceCalc(filteredSpectrum[i][0], filteredSpectrum[i+1][0]) <= precision:
       highestPeak = max(filteredSpectrum[i][1], filteredSpectrum[i+1][1])
       filteredSpectrum[i] = (filteredSpectrum[i][0], highestPeak)
