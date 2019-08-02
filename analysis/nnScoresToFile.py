49866aec78a0360a9ecdc4696ac75933eddb34f3
Alex DeCourcy <adecourc@indiana.edu>
2019-08-02 10:33:14 -0400

    minor

--- analysis/nnScoresToFile.py
+++ analysis/nnScoresToFile.py
@@ -1,6 +1,6 @@
 import argparse
 import subprocess
-import os
+import sys, os
 # cheap hack until I can figure out how to do this properly in
 # python 3.6
 sys.path.append(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0])
