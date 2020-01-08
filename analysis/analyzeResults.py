b4093840669b9f6afbfad312508f1993306d9326
unknown <adecourc@indiana.edu>
2020-01-08 13:04:11 -0500

    axis error

--- analysis/analyzeResults.py
+++ analysis/analyzeResults.py
@@ -371,8 +371,7 @@ def resultsFilter(peptideDF):
   else:
     peptideDF = peptideDF[[PEPTIDE, TITLE_SPECTRUM]]
   peptideDF = peptideDF[peptideDF[PEPTIDE] != NO_PEP]
-  print(peptideDF)
-  peptideDF[PEPTIDE] = peptideDF.apply(lambda x: x[PEPTIDE].replace('I', 'L'))
+  peptideDF[PEPTIDE] = peptideDF.apply(lambda x: x[PEPTIDE].replace('I', 'L'), axis=1)
   peptideDF = peptideDF.drop_duplicates()
 
   return peptideDF
